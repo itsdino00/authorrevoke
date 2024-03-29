535c39ceef392ffbc71267fe262754a955c7c9f0
itsdino00 <165156971+itsdino00@users.noreply.github.com>
2024-03-29 21:54:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Assuming your main App component is in App.js
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
